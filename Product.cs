f7b1131b6c34951d09910a2fd81d0b22fdf6bec2
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2025-01-31 18:35:27 -0500

    Complete 6/18 test cases

---
+++ Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group13_KrishanSingh_GouriSankar_Pradeep_Assignment2
+{
+    public class Product
+    {
+        private int prodId;
+        private string prodName;
+        private double itemPrice;
+        private int stockAmount;
+
+        public int ProdId { get => prodId; set => prodId = value; }
+        public string ProdName { get => prodName; set => prodName = value; }
+        public double ItemPrice { get => itemPrice; set => itemPrice = value; }
+        public int StockAmount { get => stockAmount; set => stockAmount = value; }
+
+        public Product(int prodId, string prodName, double itemPrice, int stockAmount)
+        {
+            //if (prodId == 0) throw new Exception("Product id can not be 0");
+            //if (prodName == "") throw new Exception("Product name can not be null");
+            //if (itemPrice <= 0) throw new Exception("Item price has to realistic");
+            //if (stockAmount == 0) throw new Exception("Atleast have some stock");
+
+            ProdId = prodId;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        public void IncreaseStock(int value)
+        {
+            stockAmount += value;
+        }
+
+        public void DecreaseStock(int value)
+        {
+            stockAmount -= value;
+        }
+    }
+}
---
+++ ProductTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Group13_KrishanSingh_GouriSankar_Pradeep_Assignment2
+{
+    [TestFixture]
+    public class ProductTest
+    {
+        // Tests for product id
+
+        // Check product id for value 5
+        [Test]
+        public void Product_ID_5_5()
+        {
+            int expected = 5;
+
+            // Arrange
+            int prodId = 5;
+            string prodName = "Assignment 2";
+            double itemPrice = 23;
+            int stockAmount = 4500;
+            Product product = new Product(prodId, prodName, itemPrice, stockAmount);
+
+            // Act
+            int actual = product.ProdId;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        // Check product id for value 20000
+        [Test]
+        public void Product_ID_20000_20000()
+        {
+            int expected = 20000;
+
+            // Arrange
+            int prodId = 20000;
+            string prodName = "Assignment 2";
+            double itemPrice = 43;
+            int stockAmount = 50000;
+            Product product = new Product(prodId, prodName, itemPrice, stockAmount);
+
+            // Act
+            int actual = product.ProdId;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        // Check product id for value 50000
+        [Test]
+        public void Product_ID_50000_50000()
+        {
+            int expected = 50000;
+
+            // Arrange
+            int prodId = 50000;
+            string prodName = "Assignment 2";
+            double itemPrice = 3201;
+            int stockAmount = 70040;
+            Product product = new Product(prodId, prodName, itemPrice, stockAmount);
+
+            // Act
+            int actual = product.ProdId;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        // Tests for product name
+
+        // TODO
+        // Product name not null
+        // Do not test for failing results. Adhere to Boundary Value Analysis
+        [Test]
+        public void Product_Name_Not_Null() { }
+
+        // Stock amount alteration
+
+        // Increase stock amount by 5000
+        [Test]
+        public void Stock_2000Add5000_7000() {
+            int expected = 7000;
+
+            // Arrange
+            int prodId = 5000;
+            string prodName = "Assignment 2";
+            double itemPrice = 3000;
+            int stockAmount = 2000;
+
+            Product product = new Product(prodId, prodName, itemPrice, stockAmount);
+            product.IncreaseStock(5000);
+
+            // Act
+            int actual = product.StockAmount;
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        // Decrease stock amount by 5000
+        [Test]
+        public void Stock_7000Minus5000_2000() {
+            int expected = 2000;
+
+            // Arrange
+            int prodId = 5000;
+            string prodName = "Assignment 2";
+            double itemPrice = 3000;
+            int stockAmount = 7000;
+
+            Product product = new Product(prodId, prodName, itemPrice, stockAmount);
+            product.DecreaseStock(5000);
+
+            // Act
+            int actual = product.StockAmount;
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+    }
+}
